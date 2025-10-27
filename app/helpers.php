6b67e3c84105697fbf75e7447d4606979478eef5
Kadmiel <kadmielchunks@gmail.com>
2025-10-28 06:48:26 +0800

    helpers update

---
+++ app/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!function_exists('getImageUrl')) {
+    /**
+     * Get image URL - handles both Cloudinary URLs and local storage
+     * 
+     * @param string|null $path
+     * @return string|null
+     */
+    function getImageUrl($path)
+    {
+        if (empty($path)) {
+            return null;
+        }
+        
+        // If it's already a full URL (Cloudinary), return as-is
+        if (str_starts_with($path, 'http://') || str_starts_with($path, 'https://')) {
+            return $path;
+        }
+        
+        // Otherwise, use storage URL for local files
+        return \Illuminate\Support\Facades\Storage::url($path);
+    }
+}
+
