d3b55265a0f5c10f3886dbee189a5fd6554e1b8d
Kadmiel <kadmielchunks@gmail.com>
2025-10-28 03:55:03 +0800

    remove tailwind CDN from all blade files

--- resources/views/main/feed.blade.php
+++ resources/views/main/feed.blade.php
@@ -8,7 +8,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="min-h-screen relative overflow-x-hidden gradient-bg">
     <div class="floating-elements fixed inset-0 pointer-events-none"></div>
