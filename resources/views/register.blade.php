69d02c3800796c8ea87893e4d24d73a44c5e3b01
Kadmiel <kadmielchunks@gmail.com>
2025-10-29 03:37:56 +0800

    freshly migrated business migrations, and removed social login properly

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -150,19 +150,6 @@
                         <span id="btn-text">Create Account</span>
                     </button>
                 </form>
-
-                <!-- Social Registration -->
-                <div class="mt-8">
-                    <div class="relative">
-                        <div class="absolute inset-0 flex items-center">
-                            <div class="w-full border-t border-gray-200"></div>
-                        </div>
-                        <div class="relative flex justify-center text-sm">
-                            <span class="px-4 bg-white text-gray-500">Or register with</span>
-                        </div>
-                    </div>
-                </div>
-
                 <!-- Login Link -->
                 <p class="text-center text-gray-600 mt-8">
                     Already have an account?
