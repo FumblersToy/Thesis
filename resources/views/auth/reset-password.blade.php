c735df137b1f79c4c85cfd0b038035c7ac5d214b
Kadmiel <kadmielchunks@gmail.com>
2025-10-29 03:33:13 +0800

    removed social login

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bandmate | Reset Password</title>
-    <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resources/css/login.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
