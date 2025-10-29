9935fed638c98ea73d55c9d65d511b286e67b55b
Kadmiel <kadmielchunks@gmail.com>
2025-10-30 03:11:39 +0800

    Commenter profile pic fixed

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -7,11 +7,16 @@ if (!function_exists('getImageUrl')) {
         }
         
         // If it's already a full URL (Cloudinary), return as is
-        if (filter_var($path, FILTER_VALIDATE_URL)) {
+        if (str_starts_with($path, 'http://') || str_starts_with($path, 'https://')) {
             return $path;
         }
         
-        // Otherwise, it's a local storage path
-        return \Illuminate\Support\Facades\Storage::url($path);
+        // If it already has /storage/, return as is
+        if (str_starts_with($path, '/storage/')) {
+            return $path;
+        }
+        
+        // Otherwise, prepend /storage/ for local files
+        return '/storage/' . $path;
     }
 }
