8c126474c46b3ac09ce7898eec4a7e84d853f9a3
Kadmiel Simbulan <kadmielchunks@gmail.com>
2025-11-21 13:50:56 +0800

    forgot password

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -41,6 +41,17 @@
                     <p class="text-gray-600">Sign in to continue your musical journey</p>
                 </div>
 
+                @if (session('status'))
+                    <div class="mb-6 bg-green-50 border-l-4 border-green-400 text-green-700 text-sm rounded-r-lg p-4">
+                        <div class="flex">
+                            <svg class="w-5 h-5 mr-2 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
+                            </svg>
+                            <span>{{ session('status') }}</span>
+                        </div>
+                    </div>
+                @endif
+
                 <div id="error-container" class="mb-6 bg-red-50 border-l-4 border-red-400 text-red-700 text-sm rounded-r-lg p-4 hidden">
                     <div class="flex">
                         <svg class="w-5 h-5 mr-2 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
