b57a8f1a8e7ea80237158f22e87ee3aa34cd47e0
Kadmiel Simbulan <kadmielchunks@gmail.com>
2025-11-21 18:50:15 +0800

    business permit

---
+++ database/migrations/2025_11_21_184931_add_business_permit_to_businesses_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('businesses', function (Blueprint $table) {
+            $table->string('business_permit')->nullable()->after('profile_picture_public_id');
+            $table->string('business_permit_public_id')->nullable()->after('business_permit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('businesses', function (Blueprint $table) {
+            $table->dropColumn(['business_permit', 'business_permit_public_id']);
+        });
+    }
+};
