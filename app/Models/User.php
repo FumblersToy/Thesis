26c81aaef8379d9c72822fec3dc50e32ddb76165
Kadmiel Simbulan <kadmielchunks@gmail.com>
2025-12-05 22:47:22 +0800

    account deletion appeal

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Notifications\CustomVerifyEmail;
+use App\Models\Admin;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
